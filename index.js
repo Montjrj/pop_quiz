92a9621dcf5f002489a8f8a866bf53267e10b4f3
Russell Montjoy <russellmontjoy@gmail.com>
2026-04-27 12:25:17 -0600

    need to fix score display

---
+++ index.js
@@ -0,0 +1,54 @@
+let score = 0; 
+
+const username = prompt("What's your name"); 
+alert(`What's up, ${username}!`); 
+
+const selectionInput = prompt(`What is 7 + 5 ? Please select one of the following options. 
+[1] 89 
+[2] 12
+`); 
+
+if(Number.isNaN(selectionInput)) {
+    alert("you must enter a number. "); 
+} else if(selectionInput > 3 || selectionInput < 1) {
+
+    alert("enter 1, 2 or 3"); 
+}else if (selectionInput === 2 ) {
+    score += 1; 
+}else {
+    console.log("Sorry, fam that's the wrong answer"); 
+}
+
+const selectionInput2 = prompt(`What is 29 + 70 ? Please select one of the following options. 
+[1] 89 
+[2] 12
+`); 
+
+if(Number.isNaN(selectionInput2)) {
+    alert("you must enter a number. "); 
+} else if(selectionInput2 > 3 || selectionInput2 < 1) {
+
+    alert("enter 1, 2 or 3"); 
+}else if (selectionInput2 === 1 ) {
+    score += 1; 
+}else {
+    console.log("Sorry, fam that's the wrong answer"); 
+}
+
+const selectionInput3 = prompt(`What is 9 + 7 ? Please select one of the following options. 
+[1] 89 
+[2] 16
+`); 
+
+if(Number.isNaN(selectionInput3)) {
+    alert("you must enter a number. "); 
+} else if(selectionInput3 > 3 || selectionInput3 < 1) {
+
+    alert("enter 1, 2 or 3"); 
+}else if (selectionInput3 === 1 ) {
+    score += 1; 
+}else {
+    console.log("Sorry, fam that's the wrong answer"); 
+}
+
+console.log(`Your final score is: ${score}`) 
